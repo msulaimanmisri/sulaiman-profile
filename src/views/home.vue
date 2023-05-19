b16aba5cb798febb3968757a0865c2e7911455c7
Muhamad Sulaiman <msulaimanmisri@gmail.com>
2023-05-19 15:22:02 +0800

    update Header

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,25 +4,30 @@
   <div class="container">
     <div class="py-5">
       <h1 class="display-2 fw-bolder text-muted text-main">
-        Your next Full Stack Web Developer
+        Your Next Full Stack Web Developer
       </h1>
 
       <p class="lead text-muted text-body">
-        With more than 7 years working experience, He's your next developer who will translate your next big idea into
-        reality.
+        With more than 7 years working experience, He's your next developer who
+        will translate your next big idea into reality.
       </p>
 
       <div class="mt-5">
-        <img src="https://sulaimanmisri.com/wp-content/uploads/2023/01/sulaiman-lead-dev.jpg"
-          class="img-fluid rounded-4 shadow" loading="lazy" alt="sulaiman-misri" style="width: 100%;
-                                                            height: 50vh;
-                                                            object-fit: cover;
-                                                            transform: scale(1) translateZ(0);
-                                                            transform-origin: 17.43% 39%;
-                                                            filter: grayscale(71%);">
+        <img
+          src="https://sulaimanmisri.com/wp-content/uploads/2023/01/sulaiman-lead-dev.jpg"
+          class="img-fluid rounded-4 shadow"
+          loading="lazy"
+          alt="sulaiman-misri"
+          style="
+            width: 100%;
+            height: 50vh;
+            object-fit: cover;
+            transform: scale(1) translateZ(0);
+            transform-origin: 17.43% 39%;
+            filter: grayscale(71%);
+          "
+        />
       </div>
-
     </div>
   </div>
 </template>
-
