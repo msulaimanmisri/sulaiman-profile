264bb33b9bb40e96d7ba641d9b409ae8edd1d1e0
Sulaiman Misri <msulaimanmisri@gmail.com>
2023-06-16 14:29:27 +0800

    Add Cari Gambo Profile

--- src/views/personal-project.vue
+++ src/views/personal-project.vue
@@ -13,152 +13,181 @@
                 In his freetime, he's building these projects. You can use if for free!
             </p>
 
-       
+
             <div class="row">
-            <div class="col-md-4 mt-4 kelasDesign">
-                <div class="card border-0 shadow-sm rounded-3 h-100">
-                    <div class="card-body">
-                        <h4 class="text-muted">
-                            Kelas Design
-                        </h4>
+                <div class="col-md-4 mt-4 kelasDesign">
+                    <div class="card border-0 shadow-sm rounded-3 h-100">
+                        <div class="card-body">
+                            <h4 class="text-muted">
+                                Kelas Design
+                            </h4>
 
-                        <p class="p-0 text-muted">
-                            Application for you to learn WordPress & Web Development
-                        </p>
+                            <p class="p-0 text-muted">
+                                Application for you to learn WordPress & Web Development
+                            </p>
+                        </div>
+
+                        <div class="card-footer border-top">
+                            <a href="https://bit.ly/daftar-kelas-design" target="_blank"
+                                class="btn btn-primary btn-stm">Visit Site</a>
+                        </div>
                     </div>
+                </div>
 
-                    <div class="card-footer">
-                        <a href="https://bit.ly/daftar-kelas-design" target="_blank" class="btn btn-primary btn-stm">Visit Site</a>
+                <div class="col-md-4 mt-4 kiraGaji">
+                    <div class="card border-0 shadow-sm rounded-3">
+                        <div class="card-body">
+                            <h4 class="text-muted">
+                                Kira Gaji
+                            </h4>
+
+                            <p class="p-0 text-muted">
+                                Application for you to calculate how much salary you gain. And How you can calculate your
+                                freelance rate.
+                            </p>
+                        </div>
+
+                        <div class="card-footer border-top">
+                            <a href="https://kira.sulaimanmisri.com" target="_blank" class="btn btn-primary btn-stm">Visit
+                                Site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mt-4 devPath">
+                    <div class="card border-0 shadow-sm rounded-3">
+                        <div class="card-body">
+                            <h4 class="text-muted">
+                                Dev Path
+                            </h4>
+
+                            <p class="p-0 text-muted">
+                                Website for you who want to learn how to code, but don't know the path. This is my personal
+                                path.
+                            </p>
+                        </div>
+
+                        <div class="card-footer border-top">
+                            <a href="https://dev.sulaimanmisri.com/" target="_blank" class="btn btn-primary btn-stm">Visit
+                                Site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mt-4 passwordGenerator">
+                    <div class="card border-0 shadow-sm rounded-3">
+                        <div class="card-body">
+                            <h4 class="text-muted">
+                                Pilihan Raya Umum 15
+                            </h4>
+
+                            <p class="p-0 text-muted">
+                                Website for you to find your choosen candidate based on Location.
+                            </p>
+                        </div>
+
+                        <div class="card-footer border-top">
+                            <a href="https://pru15.sulaimanmisri.com/" target="_blank" class="btn btn-primary btn-stm">Visit
+                                Site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mt-4 pru15">
+                    <div class="card border-0 shadow-sm rounded-3">
+                        <div class="card-body">
+                            <h4 class="text-muted">
+                                Password Generator
+                            </h4>
+
+                            <p class="p-0 text-muted">
+                                Simple website that can generate a very secure password without saving it
+                            </p>
+                        </div>
+
+                        <div class="card-footer border-top">
+                            <a href="https://pass.sulaimanmisri.com/" target="_blank" class="btn btn-primary btn-stm">Visit
+                                Site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mt-4 smPrefetch">
+                    <div class="card border-0 shadow-sm rounded-3">
+                        <div class="card-body">
+                            <h4 class="text-muted">
+                                SM Prefetch
+                            </h4>
+
+                            <p class="p-0 text-muted">
+                                A WordPress plugin that can turns your website into a blazing fast website using prefetch
+                                technology.
+                            </p>
+                        </div>
+
+                        <div class="card-footer border-top">
+                            <a href="https://wordpress.org/plugins/sm-prefetch/" target="_blank"
+                                class="btn btn-primary btn-stm">Visit Site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mt-4 smScrollbar">
+                    <div class="card border-0 shadow-sm rounded-3">
+                        <div class="card-body">
+                            <h4 class="text-muted">
+                                SM Scrollbar
+                            </h4>
+
+                            <p class="p-0 text-muted">
+                                A WordPress plugin that can make your windows browser scrollbar look nice
+                            </p>
+                        </div>
+
+                        <div class="card-footer border-top">
+                            <a href="https://wordpress.org/plugins/sm-scrollbar/" target="_blank"
+                                class="btn btn-primary btn-stm">Visit Site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mt-4 cariGambo">
+                    <div class="card border-0 shadow-sm rounded-3">
+                        <div class="card-body">
+                            <h4 class="text-muted">
+                                Cari Gambo.
+                            </h4>
+
+                            <p class="p-0 text-muted">
+                                A simple web application where you can search any Royalty-Free images
+                            </p>
+                        </div>
+
+                        <div class="card-footer border-top">
+                            <a href="https://gambo.sulaimanmisri.com" target="_blank" class="btn btn-primary btn-stm">Visit
+                                Site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mt-4 comingSoon ">
+                    <div class="card border-0 shadow-sm rounded-3">
+                        <div class="card-body">
+                            <h4 class="text-muted text-main">
+                                Coming Soon
+                            </h4>
+
+                            <p class="p-0 text-muted">
+                                Sulaiman will create more project later for you to use. Check again later.
+                            </p>
+                        </div>
+
+                        <div class="card-footer border-top">
+                            <a href="" target="_blank" class="btn btn-outline-secondary text-muted btn-stm">Later..</a>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="col-md-4 mt-4 kiraGaji">
-                <div class="card border-0 shadow-sm rounded-3">
-                    <div class="card-body">
-                        <h4 class="text-muted">
-                            Kira Gaji
-                        </h4>
-
-                        <p class="p-0 text-muted">
-                            Application for you to calculate how much salary you gain. And How you can calculate your freelance rate.
-                        </p>
-                    </div>
-
-                    <div class="card-footer">
-                        <a href="https://kira.sulaimanmisri.com" target="_blank" class="btn btn-primary btn-stm">Visit Site</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mt-4 devPath">
-                <div class="card border-0 shadow-sm rounded-3">
-                    <div class="card-body">
-                        <h4 class="text-muted">
-                            Dev Path
-                        </h4>
-
-                        <p class="p-0 text-muted">
-                            Website for you who want to learn how to code, but don't know the path. This is my personal path.
-                        </p>
-                    </div>
-
-                    <div class="card-footer">
-                        <a href="https://dev.sulaimanmisri.com/" target="_blank" class="btn btn-primary btn-stm">Visit Site</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mt-4 passwordGenerator">
-                <div class="card border-0 shadow-sm rounded-3">
-                    <div class="card-body">
-                        <h4 class="text-muted">
-                            Pilihan Raya Umum 15
-                        </h4>
-
-                        <p class="p-0 text-muted">
-                            Website for you to find your choosen candidate based on Location.
-                        </p>
-                    </div>
-
-                    <div class="card-footer">
-                        <a href="https://pru15.sulaimanmisri.com/" target="_blank" class="btn btn-primary btn-stm">Visit Site</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mt-4 pru15">
-                <div class="card border-0 shadow-sm rounded-3">
-                    <div class="card-body">
-                        <h4 class="text-muted">
-                            Password Generator
-                        </h4>
-
-                        <p class="p-0 text-muted">
-                            Simple website that can generate a very secure password without saving it
-                        </p>
-                    </div>
-
-                    <div class="card-footer">
-                        <a href="https://pass.sulaimanmisri.com/" target="_blank" class="btn btn-primary btn-stm">Visit Site</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mt-4 smPrefetch">
-                <div class="card border-0 shadow-sm rounded-3">
-                    <div class="card-body">
-                        <h4 class="text-muted">
-                            SM Prefetch
-                        </h4>
-
-                        <p class="p-0 text-muted">
-                            A WordPress plugin that can turns your website into a blazing fast website using prefetch technology.
-                        </p>
-                    </div>
-
-                    <div class="card-footer">
-                        <a href="https://wordpress.org/plugins/sm-prefetch/" target="_blank" class="btn btn-primary btn-stm">Visit Site</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mt-4 smScrollbar">
-                <div class="card border-0 shadow-sm rounded-3">
-                    <div class="card-body">
-                        <h4 class="text-muted">
-                            SM Scrollbar
-                        </h4>
-
-                        <p class="p-0 text-muted">
-                            A WordPress plugin that can make your windows browser scrollbar look nice
-                        </p>
-                    </div>
-
-                    <div class="card-footer">
-                        <a href="https://wordpress.org/plugins/sm-scrollbar/" target="_blank" class="btn btn-primary btn-stm">Visit Site</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mt-4 comingSoon ">
-                <div class="card border-0 shadow-sm rounded-3">
-                    <div class="card-body">
-                        <h4 class="text-muted text-main">
-                            Coming Soon
-                        </h4>
-
-                        <p class="p-0 text-muted">
-                          Sulaiman will create more project later for you to use. Check again later.
-                        </p>
-                    </div>
-
-                    <div class="card-footer">
-                        <a href="" target="_blank" class="btn btn-outline-secondary text-muted btn-stm">Later..</a>
-                    </div>
-                </div>
-            </div>
-        </div>
         </div>
     </div>
 </template>
